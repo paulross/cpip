b464045b03cece33d8c96cb3c4da2e46cea8c824
Paul Ross <apaulross@gmail.com>
2025-09-04 21:06:40 +0100

    Fix demo main.cpp

--- demo/src/main.cpp
+++ demo/src/main.cpp
@@ -2,7 +2,7 @@
 
 #include "user.h"
 
-int main(char **argv, int argc) {
+int main(int argc, char **argv) {
 #if defined(LANG_SUPPORT) && defined(FRENCH)
 	printf("Bonjour tout le monde\n");
 #elif defined(LANG_SUPPORT) && defined(AUSTRALIAN)
