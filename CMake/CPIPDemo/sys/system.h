ea71e32a42adca067549d88a6ac46140fa7380a5
Paul Ross <apaulross@gmail.com>
2025-09-04 21:35:30 +0100

    Add a CMake based example project based on the demo code to the CMake/ directory.
    Execution (from project root):
    
    cpipmain -l20 -o tmp/output_15 CMake/CPIPDemo/cmake-build-debug/ -J /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.0.sdk/usr/include -D __GNUC__=4 -D __x86_64__
    
    No errors.

---
+++ CMake/CPIPDemo/sys/system.h
@@ -0,0 +1,10 @@
+//
+// Created by Paul Ross on 04/09/2025.
+//
+
+#ifndef CPIPDEMO_SYSTEM_H
+#define CPIPDEMO_SYSTEM_H
+
+#define LANG_SUPPORT
+
+#endif //CPIPDEMO_SYSTEM_H
---
+++ CMake/CPIPDemo/usr/user.h
@@ -0,0 +1,14 @@
+//
+// Created by Paul Ross on 04/09/2025.
+//
+
+#ifndef CPIPDEMO_USER_H
+#define CPIPDEMO_USER_H
+
+#include <system.h>
+
+#define FRENCH FRANCAIS
+
+#define FRANCAIS
+
+#endif //CPIPDEMO_USER_H
